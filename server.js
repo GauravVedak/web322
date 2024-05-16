27daf9f9d89f5dfed16612104f4787afd7048673
Gaurav Amol Vedak <gauravvedak78@gmail.com>
2024-05-15 21:06:57 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
-console.log("Hello World");
-console.log(__dirname);
-console.log(__filename);
+/**
+*  WEB322 - Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites, friends gpt or otherwise) or distributed to other students.
+*  I understand that if caught doing so, I will receive zero on this assignment and possibly 
+*  fail the entire course.
+*  Name: Gaurav Amol Vedak
+*  Student ID: 140524232
+*  Date:  16-05-2024
+*  Vercel Web App URL:  https://vercel.com/gauravvedaks-projects/web322
+*  GitHub Repository URL:  https://github.com/GauravVedak/web322
+**/
+
+console.log("Gaurav Vedak - 140524232");
